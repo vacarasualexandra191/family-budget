9a51f7acc01dede9a20087c93eb6602e4a39a6d7
vacarasualexandra191 <vacarasu.alexandra191@gmail.com>
2026-06-22 21:48:16 +0300

    eureka and ci/cd

--- src/main/java/com/familybudget/FamilyBudgetApplication.java
+++ src/main/java/com/familybudget/FamilyBudgetApplication.java
@@ -2,8 +2,10 @@ package com.familybudget;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FamilyBudgetApplication {
 
     public static void main(String[] args) {
